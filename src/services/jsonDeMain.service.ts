c4b03c995e0a3c3614a73ab9107f2334a28bfecb
Marcos Jara <adrianjara@gmail.com>
2021-12-28 16:35:19 -0300

    1.0.30

--- src/services/jsonDeMain.service.ts
+++ src/services/jsonDeMain.service.ts
@@ -1609,7 +1609,6 @@ class JSonDeMainService {
    * @param options
    */
   private generateDatosCondicionOperacionDE_Credito(params: any, data: any) {
-
     if (!data['condicion']['credito']['tipo']) {
       throw new Error(
         'El tipo de Crédito en data.condicion.credito.tipo es obligatorio si la condicion posee créditos',
@@ -1617,10 +1616,8 @@ class JSonDeMainService {
     }
 
     if (data['condicion']['entregas'] && data['condicion']['entregas'].length > 0) {
-      if ( ! (data['condicion']['credito']['montoEntrega'] > 0)) {
-        throw new Error(
-          'Debe especificar el monto de la Entrega en condicion.credito.montoEntrega',
-        );
+      if (!(data['condicion']['credito']['montoEntrega'] > 0)) {
+        throw new Error('Debe especificar el monto de la Entrega en condicion.credito.montoEntrega');
       }
     }
 
