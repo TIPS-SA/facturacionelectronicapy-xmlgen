1a90a7fcd8dc4d0aa3ac579f76c919f0182f5fab
Marcos Jara <marcosjara@yahoo.es>
2022-08-19 09:59:17 -0400

    1.0.109

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,9 @@ class DE {
     return new Promise((resolve, reject) => {
       //Enviar Copia
       let departamentos = [];
-      for (let index = 0; index <  deService.getDepartamentos().length; index++) {
+      for (let index = 0; index < deService.getDepartamentos().length; index++) {
         const ciu = deService.getDepartamentos()[index];
-        departamentos.push({...ciu});
+        departamentos.push({ ...ciu });
       }
 
       resolve(departamentos);
@@ -46,27 +46,26 @@ class DE {
 
   consultarDistritos = (departamento: number | null): Promise<any> => {
     return new Promise((resolve, reject) => {
-            //Enviar Copia
-            let distritos = [];
-            for (let index = 0; index <  deService.getDistritos(departamento).length; index++) {
-              const ciu =  deService.getDistritos(departamento)[index];
-              distritos.push({...ciu});
-            }
-      
+      //Enviar Copia
+      let distritos = [];
+      for (let index = 0; index < deService.getDistritos(departamento).length; index++) {
+        const ciu = deService.getDistritos(departamento)[index];
+        distritos.push({ ...ciu });
+      }
+
       resolve(distritos);
     });
   };
 
   consultarCiudades = (distrito: number | null): Promise<any> => {
     return new Promise((resolve, reject) => {
-
       //Enviar Copia
       let ciudades = [];
-      for (let index = 0; index <  deService.getCiudades(distrito).length; index++) {
-        const ciu =  deService.getCiudades(distrito)[index];
-        ciudades.push({...ciu});
+      for (let index = 0; index < deService.getCiudades(distrito).length; index++) {
+        const ciu = deService.getCiudades(distrito)[index];
+        ciudades.push({ ...ciu });
       }
-    
+
       resolve(ciudades);
     });
   };
@@ -80,7 +79,7 @@ class DE {
   getDepartamento = (departamentoId: number): any => {
     let departamentos = deService.getDepartamento(departamentoId);
     if (departamentos.length > 0) {
-      return {...departamentos[0]};
+      return { ...departamentos[0] };
     } else {
       return null;
     }
@@ -89,7 +88,7 @@ class DE {
   getDistrito = (distritoId: number): any => {
     let distritos = deService.getDistrito(distritoId);
     if (distritos.length > 0) {
-      return {...distritos[0]};
+      return { ...distritos[0] };
     } else {
       return null;
     }
@@ -98,7 +97,7 @@ class DE {
   getCiudad = (ciudadId: number): any => {
     let ciudades = deService.getCiudad(ciudadId);
     if (ciudades.length > 0) {
-      return {...ciudades[0]};
+      return { ...ciudades[0] };
     } else {
       return null;
     }
